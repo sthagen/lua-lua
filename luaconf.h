f5d1e8639bf5df24c761602354218df21f796a30
Roberto I <roberto@inf.puc-rio.br>
2026-01-16 16:38:44 -0300

    New compile option LUA_COMPAT_LOOPVAR
    When on, this option makes for-loop control variables not read only.

--- luaconf.h
+++ luaconf.h
@@ -343,6 +343,13 @@
 
 
 /*
+@@ LUA_COMPAT_LOOPVAR makes for-loop control variables not read-only,
+** as they were in previous versions.
+*/
+/* #define LUA_COMPAT_LOOPVAR */
+
+
+/*
 @@ LUA_COMPAT_MATHLIB controls the presence of several deprecated
 ** functions in the mathematical library.
 ** (These functions were already officially removed in 5.3;
